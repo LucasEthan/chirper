a24a18bd389e9fdcbb95fdbed5696afeddab71f9
Lucas Ethan <asmrit1010@gmail.com>
2020-11-07 10:47:14 +0100

    Improved email callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+  before_save :downcase_email
+
   validates :name, :email, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "is not a valid email address" },
     length: { maximum: 100 }, uniqueness: { case_sensitive: false }
   validates :name, length: { minimum: 10, maximum: 250 }
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
 end

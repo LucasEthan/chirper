ba42c321ddfa3f2ecd13f5151d81de0bc9524d7b
Lucas Ethan <asmrit1010@gmail.com>
2020-11-07 10:18:35 +0100

    Added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "is not a valid email address" },
+    length: { maximum: 100 }, uniqueness: { case_sensitive: false }
+  validates :name, length: { minimum: 10, maximum: 250 }
 end

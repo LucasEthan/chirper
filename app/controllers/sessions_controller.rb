a7d7e63d297071230b716b81ca40d41f86af115e
Lucas Ethan <asmrit1010@gmail.com>
2020-12-02 14:35:38 +0100

    Added flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email])
+    if user&.authenticate(params[:session][:password])
+    else
+      flash.now[:danger] = "Incorrect username or password"
+      render :new
+    end
   end
 
   def destroy

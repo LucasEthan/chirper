933f3767559ef33fb841de2bc53b82a356b1d7fc
Lucas Ethan <asmrit1010@gmail.com>
2020-12-02 15:28:04 +0100

    Added login function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user&.authenticate(params[:session][:password])
+      log_in(user)
+      flash[:success] = "#{user.name} successfully logged in"
+      redirect_to user
     else
       flash.now[:danger] = "Incorrect username or password"
       render :new

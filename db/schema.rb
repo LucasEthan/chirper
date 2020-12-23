d635ec5df62de3875c01573a728e8f4e3146b9ee
Lucas Ethan <asmrit1010@gmail.com>
2020-12-23 14:20:31 +0100

    Added migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_19_082246) do
+ActiveRecord::Schema.define(version: 2020_12_23_131505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 2020_12_19_082246) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin"
+    t.boolean "activated", default: false, null: false
+    t.string "activation_digest"
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

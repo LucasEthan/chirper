4b25db8ac233829b2675276f8e0982370cf87361
Lucas Ethan <asmrit1010@gmail.com>
2020-12-28 18:28:48 +0100

    Added link for account activation and added validations

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,14 @@
 class AccountActivationsController < ApplicationController
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:activation_token])
+      user.activated = true
+      user.activated_at = Time.current
+      user.save!(validate: false)
+      flash[:success] = "Account #{user.name} successfully activated"
+    else
+      flash[:danger] = "Activation link is invalid"
+    end
+    redirect_to login_path
+  end
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "asmrit1010@gmail.com"
+  default from: "asmrit1010@gmail.com", reply_to: "asmrit1010@gmail.com"
   layout "mailer"
 end

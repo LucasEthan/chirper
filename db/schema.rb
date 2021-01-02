835b00d293cd5400f4e5fa1db979995bee8e7f1c
Lucas Ethan <asmrit1010@gmail.com>
2021-01-02 10:27:51 +0100

    Generated password_reset migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_131505) do
+ActiveRecord::Schema.define(version: 2021_01_02_092458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2020_12_23_131505) do
     t.boolean "activated", default: false, null: false
     t.string "activation_digest"
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

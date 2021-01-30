2ec6a3762196c58aa5ad1fdd61f15fd0cb8bee8e
Lucas Ethan <asmrit1010@gmail.com>
2021-01-30 10:27:04 +0100

    Generated chirps model and added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :chirps
+
   attr_accessor :remember_token, :activation_token, :reset_token
 
   before_validation :downcase_email, :titlecase_name, :strip_whitespaces

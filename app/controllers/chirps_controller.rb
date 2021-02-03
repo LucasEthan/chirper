7f31c18e7c47b0919087de559e031d6fecfe2ad7
Lucas Ethan <asmrit1010@gmail.com>
2021-02-03 14:46:23 +0100

    Added create action

--- app/controllers/chirps_controller.rb
+++ app/controllers/chirps_controller.rb
@@ -1,9 +1,23 @@
 class ChirpsController < ApplicationController
   before_action :logged_in_user, only: %i[create destroy]
 
-  def index
+  def create
+    @chirp = current_user.chirps.build(chirps_params)
+    if @chirp.save
+      flash[:success] = "Chirp successfully created"
+      redirect_to current_user
+    else
+      flash.now[:danger] = form_error_message(chirp)
+      render: root_path
+    end
   end
 
-  def show
+  def destroy
+  end
+
+  private
+
+  def chirps_params
+    params.require(:chirp).permit(:content)
   end
 end

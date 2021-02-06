a35eb91453b4618d508691d0a4a63536bf69e044
Lucas Ethan <asmrit1010@gmail.com>
2021-02-06 09:29:47 +0100

    Added order_by_date_desc scope for chirps

--- app/models/chirp.rb
+++ app/models/chirp.rb
@@ -5,4 +5,6 @@ class Chirp < ApplicationRecord
   validates :content, presence: true, length: {in: 1..140 }
 
   self.per_page = 5
+
+  scope :order_by_date_desc, -> { order(created_at: :desc) }
 end

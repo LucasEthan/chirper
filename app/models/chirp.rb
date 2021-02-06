58f7098f9f53d1d727a0e2ca977eccf7624469a5
Lucas Ethan <asmrit1010@gmail.com>
2021-02-06 11:47:53 +0100

    Added image validations for chirps

--- app/models/chirp.rb
+++ app/models/chirp.rb
@@ -4,6 +4,8 @@ class Chirp < ApplicationRecord
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { in: 1..140 }
+  validates :image, content_type: { in: %w[image/jpeg image/gif image/png], message: "must be a valid image format" },
+    size: { less_than: 5.megabytes, message: "should be less than 5mb" }
 
   self.per_page = 5
 

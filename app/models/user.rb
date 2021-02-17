2491ac2fb9f788ce56b53d7056ecdd350aa11ecf
Lucas Ethan <asmrit1010@gmail.com>
2021-02-17 15:05:38 +0100

    Added utility methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -71,6 +71,18 @@ class User < ApplicationRecord
     chirps
   end
 
+  def follow(other_user)
+    following << other_user 
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
   private
 
   def downcase_email

85909aa1a74804c9dce2a092e394adac4aa4af05
Lucas Ethan <asmrit1010@gmail.com>
2021-02-24 14:24:02 +0100

    Added following and unfollowing actions

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,17 @@
-class RelationshipController < ApplicationController
+class RelationshipsController < ApplicationController
+  before_action :logged_in_user
+
   def create
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
+    flash[:success] = "You have followed #{@user.name}"
+    redirect_to @user
   end
 
   def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
+    flash[:info] = "You have unfollowed #{@user.name}"
+    redirect_to @user
   end
 end
